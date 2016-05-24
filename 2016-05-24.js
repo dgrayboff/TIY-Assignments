dab1d4d902c47257c992141dfabdf019a447d47b
David <dgrayboff@gmail.com>
2016-05-24 10:26:54 -0400

    initial commit

---
+++ 2016-05-24.js
@@ -0,0 +1,10 @@
+var me = {
+  children: 2,
+	partner: "Amanda",
+	loc: "Raleigh",
+	job: "Developer"
+}
+
+var fortune = function {
+	console.log("You will be a " + me.job + "in " + me.loc + ", and married to " + me.partner + "with " + me.children + " kids.");
+}
