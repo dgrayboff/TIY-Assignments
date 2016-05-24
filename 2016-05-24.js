2d36ea806975b07daf8867417e9cab1bbb37ee24
David <dgrayboff@gmail.com>
2016-05-24 10:32:44 -0400

    edits

--- 2016-05-24.js
+++ 2016-05-24.js
@@ -1,10 +1,9 @@
 var me = {
-  children: 2,
-	partner: "Amanda",
+    children: 2,
+	partner: "Amanda ",
 	loc: "Raleigh",
-	job: "Developer"
-}
+	job: "developer"
+};
 
-var fortune = function {
-	console.log("You will be a " + me.job + "in " + me.loc + ", and married to " + me.partner + "with " + me.children + " kids.");
-}
+var fortune =
+	console.log("You will be a " + me.job + " in " + me.loc + ", and married to " + me.partner + "with " + me.children + " kids.");
