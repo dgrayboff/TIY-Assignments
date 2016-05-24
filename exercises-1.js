bd7572c60ed7b803efd1710f15508f8281799989
David <dgrayboff@gmail.com>
2016-05-23 21:19:00 -0400

    updated

---
+++ exercises-1.js
@@ -0,0 +1,69 @@
+/**
+ * For each of the following Array methods,
+
+ * 1. use them in an example
+ * 2. use console.log() to make the code run in the console!
+
+ **/
+
+// .sort()
+// sorts the items within an array, and by default, sorts values as strings in alphabetical order
+
+var iceCream = ["Strawberry", "Chocolate", "Vanilla", "Mint", "Black Raspberry"];
+
+console.log(iceCream.sort());
+
+>> [ 'Black Raspberry', 'Chocolate', 'Mint', 'Strawberry', 'Vanilla' ]
+
+
+// .concat()
+// joins values of two or more strings or arrays
+
+var bicycles = ["All-City", "Surly", "Kona"];
+var cars = ["Honda", "Toyota"];
+console.log(bicycles.concat(cars));
+
+>> [ 'All-City', 'Surly', 'Kona', 'Honda', 'Toyota' ]
+
+
+// .indexOf()
+
+
+
+// .split()
+
+
+
+// .join()
+
+
+
+// .pop()
+
+
+
+// .push()
+
+
+
+// .slice()
+
+
+
+// .splice()
+
+
+
+// .shift()
+
+
+
+// .unshift()
+
+
+
+// .filter()
+
+
+
+// .map()
