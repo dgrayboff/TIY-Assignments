4a6a3b6ec91ff745bbd433e411391dde7b159cfc
David <dgrayboff@gmail.com>
2016-05-26 10:39:52 -0400

    fixed string

--- 2016-05-26.js
+++ 2016-05-26.js
@@ -4,6 +4,6 @@ var amount = 2;
 
 for (var i = 0; i <= maxAge; i++) {
 	var total = (maxAge - currAge) * (365 * amount)
-}	console.log("You will need " + total + " to last you until the ripe old age of " + maxAge + ".");
+}	console.log("You will need " + total + " bags of chips to last you until the ripe old age of " + maxAge + ".");
 
->> You will need 51830 to last you until the ripe old age of 96.
+>> You will need 51830 bags of chips to last you until the ripe old age of 96.
