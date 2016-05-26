03135ec4a3bf2672fffa174313ca5f62ccbaa0ac
David <dgrayboff@gmail.com>
2016-05-26 10:31:45 -0400

    initial commit

---
+++ 2016-05-26.js
@@ -0,0 +1,9 @@
+var currAge = 25;
+var maxAge = 96;
+var amount = 2;
+
+for (var i = 0; i <= maxAge; i++) {
+	var total = (maxAge - currAge) * (365 * amount)
+}	console.log("You will need " + total + " to last you until the ripe old age of " + maxAge + ".");
+
+>> You will need 51830 to last you until the ripe old age of 96.
