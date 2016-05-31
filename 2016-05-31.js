c06d59f74f0def157183cc1087bbe78013199c3b
David <dgrayboff@gmail.com>
2016-05-31 10:51:42 -0400

    initial commit - rough draft

---
+++ 2016-05-31.js
@@ -0,0 +1,12 @@
+var lang = {
+	en: "Hello, world",
+    es: "Hola Mundo",
+    de: "Hallo Welt"
+}
+
+function helloWorld(lang) {
+	 for (var i = 0; i < lang.length; i++) {
+	 	return "Hello, World";
+	 }
+
+}
