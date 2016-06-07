77c4d2bc5fe273a1087aba9fbabe5d12f64de2ae
David <dgrayboff@gmail.com>
2016-06-07 10:49:00 -0400

    initial commit

---
+++ 2016-06-07.js
@@ -0,0 +1,18 @@
+var recipe = {
+	title: "Salsa",
+	servings: 4,
+	ingredients: ["tomatoes","onion","cilantro"],
+}
+
+console.log(recipe.title);
+console.log("Serves: " + recipe.servings);
+console.log("Ingredients:\n" + recipe.ingredients[0]);
+console.log(recipe.ingredients[1]);
+console.log(recipe.ingredients[2]);
+
+>> Salsa
+Serves: 4
+Ingredients:
+tomatoes
+onion
+cilantro
