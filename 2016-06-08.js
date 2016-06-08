fd2bd114903b95c7713cb0f3dc566c170b931797
David <dgrayboff@gmail.com>
2016-06-08 10:18:32 -0400

    solution

---
+++ 2016-06-08.js
@@ -0,0 +1,13 @@
+var favMovie = {
+	title: "Star Wars",
+	duration: 121,
+	stars: ["Harrison Ford", " Mark Hamill", " Carrie Fisher"]
+};
+
+function myFunction() {
+	console.log(favMovie.title + " lasts for " + favMovie.duration + " minutes. Stars: " + favMovie.stars + ".");
+}
+
+myFunction();
+
+>> Star Wars lasts for 121 minutes. Stars: Harrison Ford, Mark Hamill, Carrie Fisher.
